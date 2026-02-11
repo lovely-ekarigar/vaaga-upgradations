a4c761d9d563bc3bc7d251989f4eeeed66ea40bf
lovely-ekarigar <lovely@ekarigar.com>
2026-02-11 14:19:04 +0530

    Fix route parameter mismatches in Sliders and Tax views
    - Slider: Change ['id' => ->id] to ['slider' => ->id] for resource routes
    - Tax: Change ['id' => ->id] to ['tax' => ->id] for resource routes

--- resources/views/backend/tax/index.blade.php
+++ resources/views/backend/tax/index.blade.php
@@ -78,7 +78,7 @@
                                         }}
                                     </td>
                                     <td>
-                                        <a href="{{route('admin.tax.edit',['id'=>$item->id]) }}"
+                                        <a href="{{route('admin.tax.edit',['tax'=>$item->id]) }}"
                                            class="btn btn-xs btn-info mb-1"><i class="icon-pencil"></i></a>
 
                                         <a data-method="delete" data-trans-button-cancel="Cancel"
@@ -89,7 +89,7 @@
                                                data-toggle="tooltip"
                                                data-placement="top" title=""
                                                data-original-title="Delete"></i>
-                                            <form action="{{route('admin.tax.destroy',['id'=>$item->id])}}"
+                                            <form action="{{route('admin.tax.destroy',['tax'=>$item->id])}}"
                                                   method="POST" name="delete_item" style="display:none">
                                                 @csrf
                                                 {{method_field('DELETE')}}
