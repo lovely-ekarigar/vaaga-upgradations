3c9922879844d86847e0e42c9d0b4d391f011c75
lovely-ekarigar <lovely@ekarigar.com>
2026-02-21 20:37:09 +0530

    Hide My Courses link in user menu as requested

--- resources/views/frontend/include/user-menu.blade.php
+++ resources/views/frontend/include/user-menu.blade.php
@@ -59,6 +59,8 @@ use App\Models\UserNotification;
                     <i class="bi-chevron-right"></i>
                   </div>
                 </a>
+                {{-- My Courses link hidden as requested --}}
+                {{--
                 <a href="/user/my-courses" class="list-group-item list-group-item-action d-flex justify-content-between py-3 @if($seg2=='my-courses') active @endif">
                   <div>
                     <i class="bi-journal-bookmark me-2"></i>
@@ -68,6 +70,7 @@ use App\Models\UserNotification;
                     <i class="bi-chevron-right"></i>
                   </div>
                 </a>
+                --}}
                 <a href="/user/invoices" class="list-group-item list-group-item-action d-flex justify-content-between py-3 @if($seg2=='invoices') active @endif">
                   <div>
                     <i class="bi-credit-card me-2"></i>
